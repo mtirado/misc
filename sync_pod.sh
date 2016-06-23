aed845857a3eba297c74cccf39b55ee0877986d9
mtirado <mtirado418@gmail.com>
2016-06-23 10:25:25 -0400

    add sync_pod.sh to rsync newly installed packages to podroot home override

---
+++ sync_pod.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# sync newroot installations with home podroot directories
+# to maintain read-only access during non-core system build
+set -e
+NEWROOT=$HOME/newroot
+echo "NEWROOT -- $NEWROOT"
+rsync -av $NEWROOT/* "$HOME"
