b1b135ef7115e1cfe324ba3209303d97db2dcaab
mtirado <mtirado418@gmail.com>
2016-06-28 06:52:28 -0400

    create expected system paths

--- build_newroot.sh
+++ build_newroot.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+NEWROOT="newroot"
+rm -rf $NEWROOT
+
 echo "creating core GNU/Linux system"
-
-rm -rf newroot
 set -e
-mkdir newroot
-cp -rfv /{bin,boot,opt,etc,include,lib,libexec,man,sbin,share,var,usr} newroot
 
+mkdir $NEWROOT
+mkdir $NEWROOT/{proc,sys,tmp}
+cp -rfv /{bin,boot,opt,etc,include,lib,libexec,man,sbin,share,var,usr} $NEWROOT
 #TODO scrub system strings
 #TODO archive / checksums
 
